b8b77ce38f21851683de127b26b87187432884fc
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-11-29 16:29:48 -0300

    debug 3

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ let usersRouter = require('./api/users');
 let app = express();
 
 // view engine setup
-/* app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade'); */
+app.set('views', path.join(__dirname, 'views'));
+/* app.set('view engine', 'jade'); */
 
 app.use(cors());
 app.use(logger('dev'));
