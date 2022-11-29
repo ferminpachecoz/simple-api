c155562b8f73f41133e5465ac016b0170f2bb55b
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-11-29 16:10:30 -0300

    deploy vercel

---
+++ index.js
@@ -0,0 +1,31 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require('cors')
+
+let mainRouter = require('./api/main');
+let usersRouter = require('./api/users');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', mainRouter);
+app.use('/users', usersRouter);
+
+app.listen(3001, ()=>{
+  console.log("servidor corriendo en http://localhost:3001/")
+})
+
+module.exports = app;
