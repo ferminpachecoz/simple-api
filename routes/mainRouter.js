a463be2af353c7702bf278f66cd2c2504efa8e9f
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-12-02 13:08:19 -0300

    lorem

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mainController = require('../controllers/mainController');
 const multer = require('multer');
 let path = require('path');
 
